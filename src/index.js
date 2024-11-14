315f18fa8176cab7af26775161729d73ba44df65
hosseinmp <hosseinmpcode@gmail.com>
2024-11-14 09:12:54 +0330

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color={"blue"} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating maxRating={10} size={48} color={"#fcc419"} defaultRating={2} />
+    <StarRating maxRating={5} size={30} color={"red"} />
+    <Test />
+  </React.StrictMode>
+);
