b8521259ef8275b0e67ae73f01999129ffa1548b
hosseinmp <hosseinmpcode@gmail.com>
2024-11-20 11:29:33 +0330

    real initial

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating maxRating={10} color={"blue"} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating maxRating={10} color={"blue"} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} size={48} color={"#fcc419"} defaultRating={2} />
-    <StarRating maxRating={5} size={30} color={"red"} />
-    <Test />
+    <App />
+    {/* <StarRating maxRating={10} size={48} color={"#fcc419"} defaultRating={2} /> */}
+    {/* <StarRating maxRating={5} size={30} color={"red"} /> */}
+    {/* <Test /> */}
   </React.StrictMode>
 );
